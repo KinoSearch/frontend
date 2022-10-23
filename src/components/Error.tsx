cb5e83507e2f0f3eb64f5d1960aeeb28f4020e47
kirillsaint <kirillsaint@wtfuck.tk>
2022-10-23 15:26:37 +0600

    v3.0.1

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -6,7 +6,10 @@ function Error() {
 	return (
 		<Group>
 			<Placeholder icon={<Icon56ErrorOutline />} header="Произошла ошибка">
-				Повторите попытку позже или обратитесь в <Link href="#">поддержку</Link>
+				Повторите попытку позже или обратитесь в{" "}
+				<Link href="https://t.me/kirillsaint" target="_blank">
+					поддержку
+				</Link>
 			</Placeholder>
 		</Group>
 	);
